5109b61aaee1c4bce0338d6c5976e463dd783643
Vtoral <vtoral@ugr.es>
2022-10-11 17:22:14 +0200

    Creating structure and MainActivity

---
+++ app/src/main/java/com/example/fisoapp/presentation/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.fisoapp.presentation;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.fisoapp.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private static final int REQUEST_ENABLE_BT = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Use this check to determine whether BLE is supported on the device.  Then you can
+        // selectively disable BLE-related features.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        // Initializes a Bluetooth adapter.  For API level 18 and above, get a reference to
+        // BluetoothAdapter through BluetoothManager.
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Checks if Bluetooth is supported on the device.
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+    }
+
+
+
+
+    public void conectar(View view){
+ //       Intent intent = new Intent(this, ScanActivity.class);
+ //       startActivity(intent);
+
+    }
+
+    public void salir(View view){
+        finish();
+    }
+
+    public void configuracion(View view){
+  //      Intent intent = new Intent(this, ConfigurationActivity.class);
+  //      startActivity(intent);
+    }
+
+
+}
